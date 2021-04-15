e7a923ca6a2bf844e9a1189ea52ab048ba2fdac4
alfikr <awal.baru77@gmail.com>
2021-04-15 17:08:06 +0700

    initial commit

---
+++ src/main/java/id/jasavast/boilerplateservice/BoilerplateServiceApplication.java
@@ -0,0 +1,13 @@
+package id.jasavast.boilerplateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/jasavast/boilerplateservice/BoilerplateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.jasavast.boilerplateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
